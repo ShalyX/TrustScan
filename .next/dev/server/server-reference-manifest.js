d124e54edd8f3c514751bdb46e44dac62eddad12
ShalyX <shallomalabi89@gmail.com>
2026-03-31 23:22:41 +0100

    chore: Remove .next from tracking (it was accidentally added)

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"ThGKiMYhy4abOPjJ5lBUTtODXqH4WPtdrFOMbamiCDo=\"\n}"
--- .next/dev/static/chunks/9fa5d_next_dist_client_components_builtin_global-error_d0e15b59.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/9fa5d_next_dist_ee9ffcc2._.js"
-],
-    source: "dynamic"
-});
